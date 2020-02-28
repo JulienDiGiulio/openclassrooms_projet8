19c9ce3340430abd74473e122cee5c99f17c03a0
Julien Di Giulio <julien.digiulio@live.fr>
2020-02-28 13:21:50 +0100

    First commit

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+sudo pip install opencv-python
+sudo pip install boto3
+sudo pip install pillow
+sudo python3 -m pip install opencv-python
+sudo python3 -m pip install boto3
+sudo python3 -m pip install pillow
+
